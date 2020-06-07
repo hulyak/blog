c6fbc9b4060a6d1935d53ae2e901facb5f560ea3
hulyak <57311842+hulya95@users.noreply.github.com>
2020-06-06 18:29:11 -0700

    add express routes for new blog posts

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-//posts array for compose page
+//posts array for compose page (posts is an array of obejcts)
 const posts = [];
 
 //render ejs page 
@@ -52,9 +52,21 @@ app.get("/about", (req,res)=>{
  });
 
 
+// Express route parameters to dynamically create new pages
+// app.get("/news/:topic", function(req, res){
+//   console.log(req.params.topic);
+// });
+// localhost:3000/news/politics  // topic parameter can change 
 
-
-
+app.get("/posts/:blog" , (req, res) => {
+  const requestedTitle = req.params.blog ; 
+  //loop through the array to get dynamic information for each individual post
+  posts.forEach(function(post){
+    if(post.title === requestedTitle){
+      console.log("Match found!");
+    }
+  });
+});
 
 
 
